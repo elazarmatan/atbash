22092a483e8835f1e7f93b48aae418fbf9ee2a1c
elazar matan <elazarmata8@gmail.com>
2025-05-14 15:06:37 +0300

    the funct danger words

--- atbash/Program.cs
+++ atbash/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,8 +13,25 @@ namespace atbash
 
         static void Main(string[] args)
         {
+           
 
+        }
 
+        static int dangerousWords(string mesage, string[] danWor)
+        {
+            int counter = 0;
+            List <string> listMesage = new List <string>(mesage.Split (' '));
+            foreach (string word in listMesage)
+            {
+                for (int i =0;i < danWor.Length; i++)
+                {
+                    if (word == danWor[i])
+                    {
+                        counter++;
+                    }
+                }
+            }
+            return counter;
         }
     }
 }
